ea96d0e5c353d7b17aa1e2be245771068fc5fac8
LozinskiMatthew <lozinskimatthew0@gmail.com>
2025-05-28 22:09:21 +0200

    Added most of the project structure

---
+++ RAG/src/GeneralExceptions.py
@@ -0,0 +1,9 @@
+import sys
+import traceback
+
+class GeneralException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RAG_Task_Solver
 Advanced RAG with one of its models fine tuned, deployed with CI/CD pipeline through RESTful API
+
+conda install pip
